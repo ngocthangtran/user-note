fd25fe49ca3433f9e9ae4a8bfc4e7ae78a1b4522
thang_document <thang.tn@evotek.vn>
2022-09-14 13:13:57 +0700

    handle error exception

---
+++ src/main/java/com/leard/usernote/exception/ApiExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.leard.usernote.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+    @ExceptionHandler(value = {BadRequestException.class})
+    public ResponseEntity<Object> handleApiBadRequest(BadRequestException e){
+        ResponseError responseError = new ResponseError("ng", e.getMessage());
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+        return new ResponseEntity<>(responseError, httpStatus);
+    }
+    @ExceptionHandler(value = {NotFoundException.class})
+    public ResponseEntity<Object> handleApiNotFound(NotFoundException e){
+        ResponseError responseError = new ResponseError("ng", e.getMessage());
+        HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+        return new ResponseEntity<>(responseError, httpStatus);
+    }
+}
